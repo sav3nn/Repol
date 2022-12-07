daad62cb967136fa4d8bdbe0ee3c7ed463b09056
savenn <vadim.dflbvdflbv@yandex.ru>
2022-12-07 21:41:24 +0700

    Create ConsoleApplication3.cpp

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int year;
+	cout << "Введите год ";
+	cin >> year;
+	if (year % 4 == 0)
+	{
+		cout << "Год високосный";
+	}
+
+	else
+	{
+		cout << "Год не високосный";
+	}
+
+}
+	
